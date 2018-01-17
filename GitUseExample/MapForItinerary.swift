90279143dcb3acab6be24b61c9065dd40419516c
Joshua Flores <joshuaaaronflores@gmail.com>
2018-01-16 22:50:53 -0600

    Commit message.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Joshua Aaron Flores Stavedahl on 1/16/18.
+//  Copyright © 2018 WingMan. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
